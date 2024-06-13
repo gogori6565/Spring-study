ce1b184688162fe8bba70afa9f1dbfe68ca41e35
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2024-06-13 17:48:57 +0900

    code - #7 폼 데이터 주고 받기

---
+++ src/main/java/com/example/studyproject/dto/ArticleForm.java
@@ -0,0 +1,22 @@
+package com.example.studyproject.dto;
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    //우클릭 > Generate > Constructor : 생성자
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    //우클릭 > Generate > toString() : 출력해서 보기
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
