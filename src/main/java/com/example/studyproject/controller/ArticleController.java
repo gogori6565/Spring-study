a14135b82aee4baf88cf74a0a9e82e7dca3c4062
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2024-06-16 18:33:36 +0900

    code - #10 롬복과 리팩터링

--- src/main/java/com/example/studyproject/controller/ArticleController.java
+++ src/main/java/com/example/studyproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.studyproject.controller;
 import com.example.studyproject.dto.ArticleForm;
 import com.example.studyproject.entity.Article;
 import com.example.studyproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //로깅을 위한 어노테이션
 public class ArticleController {
 
     @Autowired //(DI-의존성 주입)스프링 부트가 미리 생성해놓은 객체를 가져다가 자동으로 연결!
@@ -23,15 +25,16 @@ public class ArticleController {
     //폼 데이터 DTO로 받고 -> Entity 변환 -> Repository 이용해 DB 저장
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        //System.out.println(form.toString()); -> 로깅으로 대체!
+        log.info(form.toString());
 
         //1. Dto를 Entity로 변환!
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
 
         //2. Repository에게 Entity를 DB안에 저장하게 함!
         Article saved = articleRepository.save(article); //.save() - CrudRepository에 정의되어 있는 기능 사용
-        System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/studyproject/dto/ArticleForm.java
+++ src/main/java/com/example/studyproject/dto/ArticleForm.java
@@ -1,26 +1,15 @@
 package com.example.studyproject.dto;
 
 import com.example.studyproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String content;
-
-    //우클릭 > Generate > Constructor : 생성자
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    //우클릭 > Generate > toString() : 출력해서 보기
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
