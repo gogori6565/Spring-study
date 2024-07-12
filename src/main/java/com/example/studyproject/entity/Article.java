8411d56c990fde919319a8d65aa7fa2b2527b6a8
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2024-07-12 16:36:31 +0900

    code - #17 CRUD와 SQL 쿼리

--- src/main/java/com/example/studyproject/entity/Article.java
+++ src/main/java/com/example/studyproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.studyproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +14,7 @@ import lombok.ToString;
 public class Article {
 
     @Id //대표값을 지정! like a 주민등록번호
-    @GeneratedValue //1, 2, 3, ... 자동 생성 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 id를 자동 생성하는 어노테이션
     private Long id;
 
     @Column
